2db6b5aadfa69a9e9d8f051001fb77c39c66be6b
prasad20001205 <prasadsamaranayake184@gmail.com>
2026-03-13 23:59:32 +0530

    Add Home component with layout structure

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,25 @@
+import Navbar from "../components/Navbar";
+import Hero from "../components/Hero";
+import AboutSection from "../components/AboutSection";
+import ServicesSection from "../components/ServicesSection";
+import ProjectsSection from "../components/ProjectsSection";
+import WhyChooseUs from "../components/WhyChooseUs";
+import ContactSection from "../components/ContactSection";
+import Footer from "../components/Footer";
+
+function Home() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <AboutSection />
+      <ServicesSection />
+      <ProjectsSection />
+      <WhyChooseUs />
+      <ContactSection />
+      <Footer />
+    </>
+  );
+}
+
+export default Home;
